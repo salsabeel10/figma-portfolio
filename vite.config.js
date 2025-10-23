a959d3a79c75a6f2c714a9932ba93156bebe210b
salsabeel10 <salsabeelibrahim10@gmail.com>
2025-10-23 14:56:59 +0400

    added case sensitive plugin vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import checker from "vite-plugin-checker";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    checker({
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"', // run ESLint with case-sensitive rule
+      },
+    }),
   ],
-})
+});
