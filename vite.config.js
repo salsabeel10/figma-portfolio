369a6c8530dfa358726015a91b69333ad29883e3
salsabeel10 <salsabeelibrahim10@gmail.com>
2025-10-23 14:59:20 +0400

    removed the plugin

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import checker from "vite-plugin-checker";
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    checker({
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}"', // run ESLint with case-sensitive rule
-      },
-    }),
   ],
 });
