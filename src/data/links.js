29d6099c4ce64a19d48362290d767191e94b4a88
salsabeel10 <salsabeelibrahim10@gmail.com>
2025-11-14 11:12:42 +0400

    changed whatsapp link msg

--- src/data/links.js
+++ src/data/links.js
@@ -1,5 +1,5 @@
 export const github="https://github.com/salsabeel10"
 export const linkedin="https://www.linkedin.com/in/salsabeel-ibrahim/"
-export const whatsapp="https://wa.link/ioujwk"
+export const whatsapp="https://wa.link/4s0efe"
 export const instagram="https://www.instagram.com/_salsabeel_ibrahim_/"
 export const resume="https://drive.google.com/file/d/1VynRwqDZicE_hkmT2TvxQOcRnFElYErp/view"
