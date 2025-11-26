60e2111f93680102e6cb42bb5ea8a7b4db186eca
salsabeel10 <salsabeelibrahim10@gmail.com>
2025-11-26 16:49:28 +0400

    changed the company projects name

--- src/data/companyProject.js
+++ src/data/companyProject.js
@@ -4,7 +4,7 @@ export const companyProjects = [
     category: "Company Project",
     title: "Al Ali Yachts",
     text: "Developed a responsive yacht rental website with booking integration and optimized SEO performance.",
-    image: "/company_projects/Al Ali Yachts.webp",
+    image: "/company_projects/Al_Ali_Yachts.webp",
     link: "https://alaliyachts.com/yacht-rental-dubai/",
   },
   {
@@ -12,7 +12,7 @@ export const companyProjects = [
     category: "Company Project",
     title: "Majestic Rent A Car",
     text: "Built a dynamic car rental platform with real-time booking, lead tracking, and Google Ads integration.",
-    image: "/company_projects/Majestic Rent a Car.webp",
+    image: "/company_projects/Majestic_Rent_a_Car.webp",
     link: "https://majesticrentacar.ae/",
   },
   {
@@ -20,14 +20,14 @@ export const companyProjects = [
     category: "Company Project",
     title: "UniQ Dubai",
     text: "Built a luxury booking site for yachts, cars, limos, helicopters, and water sports.",
-    image: "/company_projects/Uniq Dubai.webp",
+    image: "/company_projects/Uniq_Dubai.webp",
     link: "https://uniqdubai.com/exclusive-yacht-offers-dubai/",
   },{
     id: 4,
     category: "Company Project",
     title: "Xtreme Yacht",
     text: "Designed a premium yacht rental website with elegant visuals, smooth navigation, and a modern user experience.",
-    image: "/company_projects/Xtreme Yacht.webp",
+    image: "/company_projects/Xtreme_Yacht.webp",
     link: "https://xtremeyacht.com/yacht-rental-summer-offer/",
   },
   ];
