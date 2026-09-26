7887bd4febdf87dd89688f5298e0634c9e300f07
salsabeel10 <salsabeelibrahim10@gmail.com>
2026-09-26 12:59:50 +0400

    updated the company projects

--- src/data/companyProject.js
+++ src/data/companyProject.js
@@ -7,8 +7,17 @@ export const companyProjects = [
     image: "/company_projects/Al_Ali_Yachts.webp",
     link: "https://alaliyachts.com/book-a-private-luxury-yacht-rental-in-dubai-today/",
   },
+  //seariders-home-page.jpg
   {
     id: 2,
+    category: "Company Project",
+    title: "Seariders UAE",
+    text: "Built a conversion-focused wake sports offer page with special deals, WhatsApp booking, and lead tracking.",
+    image: "/company_projects/seariders-home-page.jpg",
+    link: "https://offers.searidersuae.com/",
+  },
+  {
+    id: 3,
     category: "Company Project",
     title: "Majestic Rent A Car",
     text: "Built a dynamic car rental platform with real-time booking, lead tracking, and Google Ads integration.",
@@ -16,18 +25,18 @@ export const companyProjects = [
     link: "https://majesticrentacar.ae/",
   },
   {
-    id: 3,
+    id: 4,
     category: "Company Project",
     title: "UniQ Dubai",
     text: "Built a luxury booking site for yachts, cars, limos, helicopters, and water sports.",
     image: "/company_projects/Uniq_Dubai.webp",
     link: "https://uniqdubai.com/exclusive-yacht-offers-dubai/",
   },{
-    id: 4,
+    id: 5,
     category: "Company Project",
     title: "Xtreme Yacht",
     text: "Designed a premium yacht rental website with elegant visuals, smooth navigation, and a modern user experience.",
-    image: "/company_projects/Xtreme_Yacht.webp",
-    link: "https://xtremeyacht.com/live-the-luxury-unforgettable-yacht-deals-youll-love-in-dubai/",
+    image: "/company_projects/Xtreme-yacht-offer-page.jpg",
+    link: "https://xtremeyacht.com/yacht-rental-dubai/",
   },
   ];
